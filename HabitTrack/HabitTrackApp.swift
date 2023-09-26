879025b13a345da08e09bbb6dea9fd9054a035aa
Danny Tsang <danny.tsang@me.com>
2023-09-26 15:59:28 -0500

    Initial Commit

---
+++ HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by Danny Tsang on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
